0e08af3309a3b72ff05b9a4b61fa5c83f4a30e4b
vaiozaffana <vaiozaffana>
2024-11-21 18:41:01 +0700

    feat: added back to home button

--- screens/DetailScreen.tsx
+++ screens/DetailScreen.tsx
@@ -1,11 +1,29 @@
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
-const DetailScreen = ({ route }) => {
+type RootStackParamList = {
+  Home: undefined;
+  Detail: {
+    id: string;
+    title: string;
+    description: string;
+    image: any;
+  };
+};
+
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, 'Detail'>;
+
+const DetailScreen: React.FC<DetailScreenProps> = ({ route, navigation }) => {
   const { title, description, image } = route.params;
 
-  return (
+  return ( 
     <ScrollView style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.backButton}>
+        <Image source={require('../assets/images/back.png')} />
+      </TouchableOpacity>
       <Image source={image} style={styles.image} />
       <View style={styles.content}>
         <Text style={styles.title}>{title}</Text>
@@ -19,6 +37,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
+  },
+  backButton: {
+    marginTop: 10,
+    marginBottom: 10
   },
   image: {
     width: '100%',
